ba7a9c5e3f8405a8cb9a8a61b87fbf96e64bd60b
ritaban06 <ghoshritaban2006@gmail.com>
2024-09-14 22:18:05 +0530

    Moved to Koustav's firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,20 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getFirestore} from "firebase/firestore";
-import { getAuth, GoogleAuthProvider} from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAt4jAII_2gNASUGTPfhaISnfTR6bURhOk",
-  authDomain: "recipe-85a2a.firebaseapp.com",
-  projectId: "recipe-85a2a",
-  storageBucket: "recipe-85a2a.appspot.com",
-  messagingSenderId: "944055927740",
-  appId: "1:944055927740:web:397159d4c89b9bbb9421b9"
+  apiKey: "AIzaSyCyoB8v7DiEyWfgUUST8WdhFi7OtPD9ScE",
+  authDomain: "recipe-41277.firebaseapp.com",
+  projectId: "recipe-41277",
+  storageBucket: "recipe-41277.appspot.com",
+  messagingSenderId: "565878877397",
+  appId: "1:565878877397:web:9962f03b2aced6726fc65c"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
-export const db = getFirestore(app);
-export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
+export { auth, provider };
